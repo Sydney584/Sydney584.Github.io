2df750c67baeab6dfc0ddccc53796b9dcdcc4575
sydney <sydneychase5841@gmail.com>
2021-03-28 22:07:21 -0400

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let h2 = document.createElement('h2')
 h2.textContent = "This content added by JavaScript"
-document.querySelector('body').appendChild(h2)
+document.querySelector('body').appendChild(h2);
+
+const input = document.getElementById('send email');
+
+function clickAlert() {
+  alert('Remeber to send me an Email');
+}
+
+input.addEventListener('click', clickAlert);
+        
